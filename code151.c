89209fcfcbc97f20f2a84c25a5596062f12eda34
Nikhilverma007621 <nikhilvermaanilsdt123@gmail.com>
2026-04-18 09:13:12 +0530

    Add files via upload

---
+++ code151.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int adj[MAX][MAX];   // Adjacency matrix
+int visited[MAX];
+int n, m;
+
+// DFS function
+void dfs(int node) {
+    visited[node] = 1;
+    
+    for (int i = 1; i <= n; i++) {
+        if (adj[node][i] == 1 && !visited[i]) {
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    scanf("%d %d", &n, &m);
+
+    // Initialize matrix and visited array
+    for (int i = 1; i <= n; i++) {
+        visited[i] = 0;
+        for (int j = 1; j <= n; j++) {
+            adj[i][j] = 0;
+        }
+    }
+
+    // Input edges
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+        adj[v][u] = 1;  // undirected graph
+    }
+
+    int components = 0;
+
+    // Count connected components
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            dfs(i);
+            components++;
+        }
+    }
+
+    printf("%d\n", components);
+
+    return 0;
+}
